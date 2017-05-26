7e946ec228e4411bdfebbc71421f1c6a30c4ac7e
Noah Morton <noahmorton98@gmail.com>
2017-05-25 21:12:27 -0500

    Added toggling the gui on and off, and saving text.

--- control.lua
+++ control.lua
@@ -2,27 +2,41 @@ global.notes = global.notes or {}
 
 
 function makeGui(player)
+   --make the initial frame, that holds the text box
    local frame = player.gui.center.add{type="frame",name="quill-frame",caption="Notes"}
-   frame.style.minimal_width = 800
-   frame.style.maximal_width = 800
+   frame.style.minimal_width = 850
+   frame.style.maximal_width = 850
    frame.style.minimal_height = 800
    frame.style.maximal_height = 800
 
+   frame.style.visible = false --turn it off initially
 
-   local text = frame.add{type="textfield",name="quill-textfield"}
+   local text = frame.add{type="textfield",name="quill-textfield"} --the actual place to write the notes
 
    text.style.minimal_height = 750
    text.style.minimal_width = 750
    text.style.maximal_height = 750
    text.style.maximal_width = 750
 
+   text.text = global.notes[player.index] or ""
+
+   frame.add{type="button",name="quill-close",caption="Close",tooltip="Close this window."}
 
 end
+
+--saves the text the player wrote into the global table.
+function saveText(player)
+   if global.notes[player.index] then
+      global.notes[player.index] = player.gui.center["quill-frame"]["quill-textfield"].text
+   end
+end
+
 
 function toggleGui(player)
    local guiEle = player.gui.center["quill-frame"]
 
    if guiEle.valid and guiEle.style.visible == true then
+      saveText(player)
       guiEle.style.visible = false
    else
       guiEle.style.visible = true
@@ -34,21 +48,19 @@ end
 script.on_event({defines.events.on_player_created},
    function(e)
       local player = game.players[e.player_index]
-      table.insert(global.notes,{player=player,note=""})
+      global.notes[e.player_index] = ""
 
-      player.gui.left.add{type="button",name="quill-button",caption="Notes"}.tooltip = "Click to open notes."
+      player.gui.left.add{type="button",name="quill-button",caption="Notes"}.tooltip = "Click to open/close notes."
+      makeGui(player) --make the gui
 
    end
 )
 
 script.on_event({defines.events.on_gui_click},
    function(e)
-      if e.element.name == "quill-button" then
-
+      if e.element.name == "quill-button" or e.element.name == "quill-close" then
          local player = game.players[e.player_index]
-
-         makeGui(player)
-         game.print("opened notes.")
+         toggleGui(player)
       end
    end
 )
